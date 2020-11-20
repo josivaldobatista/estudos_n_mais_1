c119d6c5ec920c3fd0492cd9bf33dade9eac514c
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-11-19 21:24:07 -0300

    Problem N+1 in Spring Data JPA sample.

--- src/main/java/com/example/demo/resources/ProductResource.java
+++ src/main/java/com/example/demo/resources/ProductResource.java
@@ -1,5 +1,8 @@
 package com.example.demo.resources;
 
+import com.example.demo.dto.ProductDTO;
+import com.example.demo.service.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,9 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.dto.ProductDTO;
-import com.example.demo.service.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -24,7 +24,6 @@ public class ProductResource {
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "size", defaultValue = "10") Integer size
 			) {
-		
 		PageRequest pageRequest = PageRequest.of(page, size);
 		Page<ProductDTO> list = service.find(pageRequest);
 		return ResponseEntity.ok(list);
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,14 +1,16 @@
 package com.example.demo.service;
 
+import java.util.stream.Collectors;
+
+import com.example.demo.dto.ProductDTO;
+import com.example.demo.entities.Product;
+import com.example.demo.repositories.ProductRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.example.demo.dto.ProductDTO;
-import com.example.demo.entities.Product;
-import com.example.demo.repositories.ProductRepository;
 
 @Service
 public class ProductService {
@@ -18,7 +20,8 @@ public class ProductService {
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> find(PageRequest pageRequest) {
-		Page<Product> list = repository.findAll(pageRequest);
-		return list.map(x -> new ProductDTO(x));
+		Page<Product> page = repository.findAll(pageRequest);
+		repository.findProductsCategories(page.stream().collect(Collectors.toList())); // <- Apenas para exemplo.
+		return page.map(x -> new ProductDTO(x));
 	}
 }
